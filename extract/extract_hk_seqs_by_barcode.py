46c162461a018ca90ca6ab0508808acda08da5e0
tangjon1 <81594929+tangjon1@users.noreply.github.com>
2025-11-10 22:33:42 -0800

    Optimization

--- extract/extract_hk_seqs_by_barcode.py
+++ extract/extract_hk_seqs_by_barcode.py
@@ -11,7 +11,6 @@ import gzip
 
 # Multithreading
 import concurrent.futures
-import threading
 
 # Type annotations (and NamedTuple)
 from typing import NamedTuple, Any, IO, Generator, TextIO
@@ -21,7 +20,7 @@ from io import TextIOWrapper
 from os import path
 
 # Establish the motif region to extract tha barcode from
-BARCODE_MOTIF = r'((GTCGCTGCCGAACAGC)(........................)(AGGAGAAGAGCGCACG)){e<4}'
+BARCODE_MOTIF: regex.Pattern = regex.compile(r'((GTCGCTGCCGAACAGC)(........................)(AGGAGAAGAGCGCACG)){e<4}', regex.BESTMATCH | regex.IGNORECASE)
 
 # Complement table for reverse complementing
 COMPLEMENT_TABLE = str.maketrans("ATGC", "TACG")
@@ -70,8 +69,8 @@ def validate_args(args: argparse.Namespace) -> None:
     '''Validate the user's configuration of arguments; raises an error if the current configuration leads to an incompatibility'''
     if args.threads < 1:
         raise argparse.ArgumentError(args.threads, "Number of threads must be ≥1")
-    if args.max_pending <= args.threads:
-        raise argparse.ArgumentError(args.max_pending, "Maximum pending processes should exceed the number of threads")
+    if args.max_pending < 1:
+        raise argparse.ArgumentError(args.max_pending, "Maximum pending processes multiplier must be ≥1")
 
 
 def get_fastq_seqs(fastq_file_handle: IO[Any] | TextIO) -> Generator[FastqSeqs, None, None]:
@@ -113,11 +112,6 @@ class BarcodeSeqExtractor:
         # Store the flag to determine output format (FASTQ or FASTA)
         self.fastq_export = fastq_export
 
-        # Threading locks to prevent race conditions
-        self.barcode_freq_dict_lock: threading.Lock = threading.Lock()
-        self.stats_dict_lock: threading.Lock = threading.Lock()
-        self.output_write_lock: threading.Lock = threading.Lock()
-
 
     def process_seqs(self, seqs: FastqSeqs) -> ProcessedSeqs:
         '''
@@ -136,13 +130,13 @@ class BarcodeSeqExtractor:
         has_endseq = False
 
         # Search the nt sequence for the motif
-        nt_seq: str = seqs.nt.upper()
-        motif_match: regex.Match[str] | None = regex.search(BARCODE_MOTIF, nt_seq, regex.BESTMATCH)
+        nt_seq: str = seqs.nt
+        motif_match: regex.Match[str] | None = regex.search(BARCODE_MOTIF, nt_seq)
 
         # Check the reverse strand if there was no match on the forward strand
         if motif_match is None:
             nt_seq = reverse_complement(nt_seq)
-            motif_match = regex.search(BARCODE_MOTIF, nt_seq, regex.BESTMATCH)
+            motif_match = regex.search(BARCODE_MOTIF, nt_seq)
             if motif_match is not None:
                 is_reverse = True
 
@@ -164,41 +158,27 @@ class BarcodeSeqExtractor:
                         return_qual_seq = seqs.qual[:barcode_index][(startseq_index + 6):endseq_index]
 
         return ProcessedSeqs(barcode, return_nt_seq, return_qual_seq, (motif_match is not None), is_reverse, has_startseq, has_endseq)
-        
 
-    def worker(self, seqs: FastqSeqs, output_file: TextIO) -> None:
-        '''Process the FASTQ sequences and handle the concurrency to update the dictionaries and write to the output file'''
-        # Process the passed "raw" sequence
-        processed_seqs: ProcessedSeqs = self.process_seqs(seqs)
 
-        # Track the stats in the stats dictionary
-        with self.stats_dict_lock:
-            self.stats_dict['has_barcode_motif'] += processed_seqs.has_barcode_motif
-            self.stats_dict['is_reverse'] += processed_seqs.is_reverse
-            self.stats_dict['no_startseq'] += processed_seqs.has_barcode_motif and not processed_seqs.has_startseq
-            self.stats_dict['no_endseq'] += processed_seqs.has_barcode_motif and not processed_seqs.has_endseq and processed_seqs.has_startseq
-            self.stats_dict['unambiguous_site'] += processed_seqs.nt_seq is not None
-        
-        # Update that barcode in the dictionary of sequences
+    def handle_processed_seqs_result(self, processed_seqs: ProcessedSeqs, output_file: TextIO) -> None:
+        '''Update current state and write to output file given the results of a processed record'''
+        # Update stats dictionary with provided results
+        self.stats_dict['has_barcode_motif'] += processed_seqs.has_barcode_motif
+        self.stats_dict['is_reverse'] += processed_seqs.is_reverse
+        self.stats_dict['no_startseq'] += processed_seqs.has_barcode_motif and not processed_seqs.has_startseq
+        self.stats_dict['no_endseq'] += processed_seqs.has_barcode_motif and not processed_seqs.has_endseq and processed_seqs.has_startseq
+        self.stats_dict['unambiguous_site'] += processed_seqs.nt_seq is not None
+
+        # Update the barcode frequency
         if processed_seqs.barcode is not None:
-            with self.barcode_freq_dict_lock:
-                self.barcode_freq_dict.setdefault(processed_seqs.barcode, 0)
-                self.barcode_freq_dict[processed_seqs.barcode] += 1
+            self.barcode_freq_dict[processed_seqs.barcode] = self.barcode_freq_dict.get(processed_seqs.barcode, 0) + 1
 
-        # If the sequences could be successfully processed, update the dictionary and write the record to the output file
+        # Export to the appropriate format
         if processed_seqs.nt_seq is not None:
-    
-            # Render the sequences into the appropriate format and export
-            write_record: str = ""
-
-            # Use FASTQ format if flag is set, otherwise FASTA
             if self.fastq_export:
-                write_record = f"@{processed_seqs.barcode}\n{processed_seqs.nt_seq}\n+\n{processed_seqs.qual_seq}\n"
+                output_file.write(f"@{processed_seqs.barcode}\n{processed_seqs.nt_seq}\n+\n{processed_seqs.qual_seq}\n")
             else:
-                write_record = f">{processed_seqs.barcode}\n{processed_seqs.nt_seq}\n"
-
-            with self.output_write_lock:
-                output_file.write(write_record)
+                output_file.write(f">{processed_seqs.barcode}\n{processed_seqs.nt_seq}\n")
 
 
     def extract_seqs(self, num_threads: int, max_pending: int):
@@ -206,9 +186,9 @@ class BarcodeSeqExtractor:
         # Establish output file
         output_filepath_sequences = path.join(self.output_dir_path, f"{self.input_file_basename}.fast{'q' if self.fastq_export else 'a'}")
 
-        # Run 
+        # Run main logic
         with (
-            concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor,
+            concurrent.futures.ProcessPoolExecutor(max_workers=num_threads) as executor,
             open_read(self.input_file_path) as input_file,
             open(output_filepath_sequences, 'w') as output_file_sequences
         ):
@@ -220,14 +200,18 @@ class BarcodeSeqExtractor:
                 # Keep no more than max_pending futures in memory
                 if len(futures) >= max_pending:
                     # Wait for one future to complete before adding a new one
-                    _, futures = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
+                    done, futures = concurrent.futures.wait(futures, return_when=concurrent.futures.FIRST_COMPLETED)
+
+                    # As completed, immediately handle the result
+                    for future in done:
+                        self.handle_processed_seqs_result(future.result(), output_file_sequences)
 
                 # Process the record
-                futures.add(executor.submit(self.worker, fastq_seqs, output_file_sequences))
+                futures.add(executor.submit(self.process_seqs, fastq_seqs))
 
-            # Wait for remaining tasks to finish
-            for _ in concurrent.futures.as_completed(futures):
-                pass
+            # Wait for any remaining records to be processed and handle them
+            for future in concurrent.futures.as_completed(futures):
+                self.handle_processed_seqs_result(future.result(), output_file_sequences)
 
             self.total_records: int = i + 1
         
